25c3015e9c58d4a6433f89b6ed85f60fbc14d734
kjs624 <kjs624@gmail.com>
2025-12-11 12:23:01 +0900

    fix: Force Webpack bundler with --webpack flag to avoid Turbopack UTF-8 bug

--- OneDrive/문서/고준성- 바이블 코딩/memo-talk/next.config.ts
+++ OneDrive/문서/고준성- 바이블 코딩/memo-talk/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // Disable Turbopack to avoid UTF-8 bug with Korean characters in path
-    turbo: undefined,
-  },
 };
 
 export default nextConfig;
